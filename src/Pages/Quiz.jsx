452a580a48ff83d82bdee10ee97af4803e14ef94
David <dehong.xu@outlook.com>
2024-01-27 20:50:10 +1000

    Finalised overall styling for the quiz interface

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,19 +1,177 @@
 import './Quiz.css';
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState, useEffect } from 'react';
+
 
 export default function Quiz()
 {
+    // variable setup
+    const quizUrl = 'https://opentdb.com/api.php?amount=5&type=multiple';
+    const [quiz, setQuiz] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // function to combine incorrect and correct answer into one
+    async function combineAnswers(incorrectAnswers, correctAnswer)
+    {
+        let allAnswers = [...incorrectAnswers];
+        allAnswers.push(correctAnswer);
+        allAnswers.sort(() => Math.random() - 0.5);
+        return allAnswers;
+    }
+
+    // fetch data from api and set it into a state
+    useEffect(() =>
+    {
+        const fetchData = async () => 
+        {
+            setLoading(true);
+            try 
+            {
+                const response = await fetch(quizUrl);
+                const quizData = await response.json();
+                
+                // processed the raw data and return a data with combined answers
+                const processedData = await Promise.all(quizData.results.map(async (question) =>
+                {
+                    question.answers = await combineAnswers(question.incorrect_answers, question.correct_answer);
+                    return question;
+                }));
+
+                setQuiz(processedData);
+            }
+            catch (error)
+            {
+                console.error('Error fetching data: ', error);
+            }
+            finally
+            {
+                setLoading(false);
+            }
+        };
+            
+        fetchData();
+    }, []);
+
+    // converts html to regular characters
+    function removeCharacters(question) 
+    {
+        return question
+            .replace(/&quot;/g, "\"")
+            .replace(/&rsquo;/g, "'")
+            .replace(/&#039;/g, "'")
+            .replace(/&amp;/g, "&");
+    }
+
     return (
         <Container className="quiz-container">
             <Container className='quiz'>
                 <h1 className='heading'>Quizzy Time</h1>
-                <hr className='divider'/>
-                <h2>Q1. Example quiz question here</h2>
-                <h3>Answer 1</h3>
-                <h3>Answer 2</h3>
-                <h3>Answer 3</h3>
-                <h3>Answer 4</h3>
-            </Container>
+                <hr className='divider'/>      
+                {loading ? 
+                (
+                    <Container>
+                        <h1>Loading...</h1>
+                        <Spinner animation='border' variant='light'/>
+                    </Container>
+                ) : 
+                (
+                    quiz && quiz.length > 0 ? 
+                    (
+                        <>
+                            <p>Category: {quiz[0].category} | Difficulty: {quiz[0].difficulty}</p>
+                            <Row className='align-items-center'>
+                                <Col xs={3} sm={2}>
+                                    <p>Progress:</p>
+                                </Col>
+                                <Col xs={9} sm={10}>
+                                    <ProgressBar now={40} label={`${2}/${5}`}/>
+                                </Col>
+                            </Row>
+                            <Row className='align-items-center'>
+                                <Col xs={6} md={8} className='text-start'>
+                                    <p>1 of 5 questions</p>
+                                </Col>
+                                <Col xs={6} md={4}>
+                                    <Container className="d-flex justify-content-end align-items-center">
+                                        <Container className="timer d-inline-flex align-items-center">
+                                            <img 
+                                                src="/img/timer.png"
+                                                alt='timer-icon'
+                                            />
+                                            <span>10s</span>
+                                        </Container>
+                                        <Button className='add-time-btn'>
+                                            <img src='/img/add.png'
+                                                alt='add-more-time'
+                                                width='24'
+                                                height='24'
+                                            />
+                                        </Button>
+                                    </Container>
+                                </Col>
+                            </Row>
+                            <hr className='divider'/> 
+                            <h2 className='heading'>Q. {removeCharacters(quiz[0].question)}</h2>
+                            <Row className='align-items-center'>
+                                <Col xs={6} className='text-end'>
+                                    <p>3/3 hints remaining</p>
+                                </Col>
+                                <Col xs={6}>
+                                    <Button className='hint-btn'>
+                                        <img 
+                                            src='/img/hint.png'
+                                            alt='hint-icon'
+                                            height='24'
+                                            width='24'
+                                        />
+                                        <span style={{ paddingLeft: '5px' }}>Hints</span>
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Button variant='outline-light'>{quiz[0].answers[0]}</Button>{' '}
+                            <Button variant='outline-light'>{quiz[0].answers[1]}</Button>{' '}
+                            <Button variant='outline-light'>{quiz[0].answers[2]}</Button>{' '}
+                            <Button variant='outline-light'>{quiz[0].answers[3]}</Button>{' '}
+                            <hr className='divider'/>  
+                            <Row className='align-items-center'>
+                                <Col xs={4} className='d-flex justify-content-end'>
+                                    <Button className='prev-btn'>
+                                        <img 
+                                            src='/img/prev.png'
+                                            alt='prev-icon'
+                                            height='24'
+                                            width='24'
+                                        />
+                                        <span style={{ paddingLeft: '5px' }}>Prev</span>   
+                                    </Button>
+                                </Col>
+                                <Col xs={4} className='d-flex justify-content-center'>
+                                    <Button className='submit-btn'>
+                                        Submit
+                                    </Button>
+                                </Col>
+                                <Col xs={4} className='d-flex justify-content-start'>
+                                    <Button className='next-btn'>
+                                        <span style={{ paddingRight: '5px' }}>Next</span>
+                                        <img 
+                                            src='/img/next.png'
+                                            alt='next-icon'
+                                            height='24'
+                                            width='24'
+                                        />
+                                    </Button>
+                                </Col>
+                            </Row> 
+                        </>
+                    ) :
+                    ( <h1>No quiz data available.</h1> )
+                )}     
+            </Container>               
         </Container>
     )
 }
